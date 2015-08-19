a7b9d944b4723d7b8e5a9a7d7db1b45769517280
Joseph A. Lutz <joseph.lutz@novatechweb.com>
2015-08-19 13:13:08 -0500

    Initial import of docker image configuration
    Signed-off-by: Joseph A. Lutz <joseph.lutz@novatechweb.com>

---
+++ docker-entrypoint.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+CONFIG_PATH=/etc/exim4
+
+# if the config directory is empty load up the debain default configuration files
+if [[ "$(cd ${CONFIG_PATH};ls -1)" == '' ]]; then
+    echo >&2 "Use the default configuration"
+    tar -xavf /demian.default.exim4.config.tar.gz -C /etc 
+fi
+
+# Make certain the directory exists for storing the PID files
+[ -e /var/run/exim4 ] || \
+    install -d -oDebian-exim -gDebian-exim -m750 /var/run/exim4
+
+# Specify the permissions of the mounted volumes and their subdirectories
+chown Debian-exim:Debian-exim ${CONFIG_PATH}
+chown -R Debian-exim:Debian-exim /var/spool/exim4
+chown -R Debian-exim:adm /var/log/exim4
+chown -R root:mail /var/mail
+
+if   [[ -f ${CONFIG_PATH}/update-exim4.conf.conf ]] \
+  && [[ -f ${CONFIG_PATH}/exim4.conf.template ]] \
+  && [[ ! -f ${CONFIG_PATH}/exim4.conf ]]
+then
+    # run the debian utility to update/generate the configuration
+    $(which update-exim4.conf) --verbose
+fi
+
+# Check configuration
+if ! /usr/sbin/exim4 -bV > /dev/null ; then
+  echo >&2 "Warning! Invalid configuration file for exim4. Exiting."
+  exit 1
+fi
+
+case ${1} in
+    exim4)
+        shift
+        echo >&2 "starting exim4"
+        exec /usr/sbin/exim4 ${@:--bdf -q30m}
+        ;;
+
+    backup)
+        cd ${CONFIG_PATH}
+        /bin/tar \
+            --create \
+            --preserve-permissions \
+            --same-owner \
+            --directory=${CONFIG_PATH} \
+            --to-stdout \
+            ./*
+        ;;
+
+    restore)
+        echo >&2 "Remove previous config"
+        rm -rf ${CONFIG_PATH}/*
+        echo >&2 "Extract the archive"
+        /bin/tar \
+            --extract \
+            --preserve-permissions \
+            --preserve-order \
+            --same-owner \
+            --directory=${CONFIG_PATH} \
+            -f -
+        echo >&2 "Set permissions"
+        chown -R Debian-exim:Debian-exim ${CONFIG_PATH}
+        ;;
+
+    *)
+        # run some other command in the docker container
+        exec "$@"
+        ;;
+esac
