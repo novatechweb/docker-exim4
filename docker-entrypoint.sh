9993e65677a21641c5b3d496596b493eafd528d3
Joseph A. Lutz <joseph.lutz@novatechweb.com>
2015-10-05 14:43:05 -0500

    update /etc/mailname with HOSTNAME
    Signed-off-by: Joseph A. Lutz <joseph.lutz@novatechweb.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,9 +22,13 @@ if   [[ -f ${CONFIG_PATH}/update-exim4.conf.conf ]] \
   && [[ -f ${CONFIG_PATH}/exim4.conf.template ]] \
   && [[ ! -f ${CONFIG_PATH}/exim4.conf ]]
 then
-    # run the debian utility to update/generate the configuration
+    # set the hostname
+    [ -e /etc/mailname ] \
+        && rm -rf /etc/mailname
+    echo "${HOSTNAME}" > /etc/mailname
     LOCAL_IP_ADDR=$(grep "[ \t]*${HOSTNAME}[ \t]*" /etc/hosts|cut -f 1)
     sed -i 's|dc_local_interfaces=.*|dc_local_interfaces='"'${LOCAL_IP_ADDR}'"'|' ${CONFIG_PATH}/update-exim4.conf.conf
+    # run the debian utility to update/generate the configuration
     $(which update-exim4.conf) --verbose
 fi
 
